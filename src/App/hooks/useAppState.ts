c61a3893e9c284228e5dc5c6f99228c82cda27f2
Yuval Shimoni <yuvals@welldone-software.com>
2020-05-12 14:05:13 +0300

    small test

--- src/App/hooks/useAppState.ts
+++ src/App/hooks/useAppState.ts
@@ -4,6 +4,7 @@ import { AppState } from "App/state/types";
 
 export const useAppState = (): AppState => {
   const [cart, cartDispatch] = useReducer(cartReducer, []);
+  console.log(cart)
 
   return {
     cart,
