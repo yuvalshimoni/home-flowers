431dbe1a72ef2e943d0f2d9250fdfee515bec6f3
Omer Prizner <omer.prizner@gmail.com>
2020-05-12 12:37:30 +0300

    remove unused code

--- src/order/components/Order.tsx
+++ src/order/components/Order.tsx
@@ -1,10 +1,9 @@
-import React, { useReducer, useCallback } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import PickProducts from './PickProducts';
 import Details from './Details';
 import Payment from './Payment';
 import Summary from './Summary';
-import { Cart, CartItemType } from '../types';
 
 // const orderShape = {
 //   cart = [],
@@ -18,48 +17,6 @@ import { Cart, CartItemType } from '../types';
 
 const Wrapper = styled.div`
 `;
-
-
-type Action =
-  {
-    type: 'UPDATE_CART_ITEM';
-    payload: CartItemType;
-  }
-  | {
-    type: 'REMOVE_ITEM_FROM_CART';
-    payload: {
-      productId: CartItemType['productId'];
-    };
-  };
-
-
-const cartReducer = (state: Cart, action: Action) => {
-  switch (action.type) {
-    case 'UPDATE_CART_ITEM':
-      {
-        const { productId, amount, price } = action.payload;
-
-        return state.some(p => p.productId === productId)
-          ? state.map(product => {
-            if (product.productId === productId) {
-              return { ...product, amount };
-            } else {
-              return product;
-            }
-          }) : state.concat([{ productId, amount: 1, price }]);
-      }
-    case 'REMOVE_ITEM_FROM_CART':
-      {
-        const productId = action.payload.productId;
-
-        return state.filter(p => p.productId === productId);
-      }
-    default:
-      return state;
-  }
-};
-
-type UpdateCart = ({ productId, amount, price }: CartItemType) => void;
 
 const Order = () => {
   return (
