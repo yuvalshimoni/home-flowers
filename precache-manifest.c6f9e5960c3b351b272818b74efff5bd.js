5db3421c948ddb3130ab952da2fcb5c42f167111
Yuval Shimoni <yuvals@welldone-software.com>
2020-06-03 21:06:06 +0300

    Updates

---
+++ precache-manifest.c6f9e5960c3b351b272818b74efff5bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1194a26bb9ee733cb796d5967fedbbe8",
+    "url": "/home-flowers/index.html"
+  },
+  {
+    "revision": "06f1917cee334124bd28",
+    "url": "/home-flowers/static/js/2.07a4229c.chunk.js"
+  },
+  {
+    "revision": "6212ea7a5da39d7d7b62902c84a2d099",
+    "url": "/home-flowers/static/js/2.07a4229c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3057d5541ad63ff4f951",
+    "url": "/home-flowers/static/js/main.1b55d437.chunk.js"
+  },
+  {
+    "revision": "3db9e81e7644fd3ada5d",
+    "url": "/home-flowers/static/js/runtime-main.1edffb17.js"
+  },
+  {
+    "revision": "66d2d4bf669250331a8fc70305db524a",
+    "url": "/home-flowers/static/media/logo.66d2d4bf.png"
+  }
+]);
